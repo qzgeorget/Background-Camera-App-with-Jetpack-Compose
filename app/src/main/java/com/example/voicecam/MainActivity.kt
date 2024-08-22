8d5a4f5791fe17d1fd60fb5b8a89e36c9a9d7aae
driplord3000 <george.10152015@gmail.com>
2024-08-22 11:49:33 +0100

    Base service template

---
+++ app/src/main/java/com/example/voicecam/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.voicecam
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+class MainActivity: ComponentActivity() {
+
+    private var isBound: Boolean = false
+    private var myService: VideoRecordingService? = null
+
+    private val connection= object: ServiceConnection{
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as VideoRecordingService.LocalBinder
+            myService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            isBound = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            Surface {
+                MyApp()
+            }
+        }
+        Intent(this, VideoRecordingService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (isBound) {
+            unbindService(connection)
+            isBound = false
+        }
+    }
+
+    @Composable
+    fun MyApp() {
+        var buttonClicked by remember { mutableStateOf(false) }
+
+        Button(onClick={
+            buttonClicked = true
+            myService?.logMessage()
+        }) {
+            Text(text = if (buttonClicked) "Service Called" else "Call Service")
+        }
+    }
+
+}
--- app/src/main/java/com/example/voicecam/VideoRecordingService.kt
+++ app/src/main/java/com/example/voicecam/VideoRecordingService.kt
@@ -1,4 +1,30 @@
 package com.example.voicecam
 
-class VideoRecordingService {
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+
+class VideoRecordingService: Service(){
+
+    private val binder = LocalBinder()
+
+    inner class LocalBinder: Binder() {
+        fun getService(): VideoRecordingService = this@VideoRecordingService
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.d("testing", "Service Bound")
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d("testing", "Service Unbound")
+        return super.onUnbind(intent)
+    }
+
+    fun logMessage() {
+        Log.d("testing", "Service is doing something...")
+    }
 }
