bb5e35cd6ab1763290c11af87674709cc18799bd
driplord3000 <george.10152015@gmail.com>
2024-09-12 19:37:29 +0100

    navigation kind of works but location is set to sanfrancisco

--- app/src/main/java/com/example/voicecam/MainActivity.kt
+++ app/src/main/java/com/example/voicecam/MainActivity.kt
@@ -33,17 +33,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.Locale
 
 class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
 
@@ -53,7 +47,7 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
     private var isBoundAudio: Boolean = false
     private var audioService: SpeechRecognitionService? = null
 
-    private val videoConnection= object: ServiceConnection{
+    private val videoConnection = object: ServiceConnection {
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
             val binder = service as VideoRecordingService.LocalBinder
             videoService = binder.getService()
@@ -65,7 +59,7 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
         }
     }
 
-    private val audioConnection= object: ServiceConnection{
+    private val audioConnection = object: ServiceConnection {
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
             val binder = service as SpeechRecognitionService.LocalBinder
             audioService = binder.getService()
@@ -84,7 +78,7 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             Surface {
                 GetPermissions()
             }
@@ -98,7 +92,7 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
 
         mapView = MapView(this)
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+        mapView.getMapAsync(this) // Set the callback to `this` which is `OnMapReadyCallback`
     }
 
     override fun onResume() {
@@ -134,19 +128,19 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     @Composable
-    fun GetPermissions(){
+    fun GetPermissions() {
         val permissions = listOf(
             android.Manifest.permission.RECORD_AUDIO,
             android.Manifest.permission.CAMERA,
             android.Manifest.permission.POST_NOTIFICATIONS
         )
 
-        var allPermissionGranted by remember { mutableStateOf(false)}
+        var allPermissionGranted by remember { mutableStateOf(false) }
 
         val launcher = rememberLauncherForActivityResult(
             contract = ActivityResultContracts.RequestMultiplePermissions()
-        ) {permissionsResult ->
-            allPermissionGranted = permissionsResult.values.all{it}
+        ) { permissionsResult ->
+            allPermissionGranted = permissionsResult.values.all { it }
         }
 
         LaunchedEffect(Unit) {
@@ -164,8 +158,8 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
         var searchQuery by remember { mutableStateOf("") }
         val context = LocalContext.current
 
-        Column(){
-            Button(onClick={
+        Column() {
+            Button(onClick = {
                 if (isLogging) {
                     videoService?.stopLogging()
                     audioService?.isServiceListening = false
@@ -188,78 +182,32 @@ class MainActivity: ComponentActivity(), keywordListener, OnMapReadyCallback {
             )
 
             Button(onClick = {
-                searchLocation(context, searchQuery)
+                lifecycleScope.launch {
+                    searchLocationAndDrawRoute(context, searchQuery, googleMap)
+                }
             }, modifier = Modifier.padding(16.dp)) {
-                Text("Search")
+                Text("Search and Navigate")
             }
 
             Box(modifier = Modifier.fillMaxSize()) {
-                GoogleMapComponent()
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { mapView }
+                )
             }
         }
     }
 
-    override fun onMapReady(map: GoogleMap) {
-        googleMap = map
-        val location = LatLng(37.7749, -122.4194)
-        googleMap.addMarker(MarkerOptions().position(location).title("Marker in San Francisco"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-    }
-
-    private fun searchLocation(context: Context, location: String){
-        if (location.isEmpty()){
-            Toast.makeText(context, "Please enter a location to search", Toast.LENGTH_SHORT).show()
-            return
-        }
-        lifecycleScope.launch(Dispatchers.IO){
-            try {
-                val geocoder = android.location.Geocoder(context, Locale.getDefault())
-                val addresses = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-                    geocoder.getFromLocationName(location, 1) // New suspend function in API level 33
-                } else {
-                    @Suppress("DEPRECATION")
-                    geocoder.getFromLocationName(location, 1)
-                }
-                if (addresses != null && addresses.isNotEmpty()) {
-                    val address = addresses[0]
-                    val latlng = LatLng(address.latitude, address.longitude)
-
-                    lifecycleScope.launch(Dispatchers.Main){
-                        googleMap.clear()
-                        googleMap.addMarker(MarkerOptions().position(latlng).title(location))
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 12f))
-                    }
-                }else {
-                    launch(Dispatchers.Main){
-                        Toast.makeText(context, "Location not found", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            } catch (e:Exception) {
-                Log.e("MapError", "Geocoding failed", e)
-                launch(Dispatchers.Main) {
-                    Toast.makeText(context, "Error finding location", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun GoogleMapComponent(){
-        val context = LocalContext.current
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = {
-                mapView.apply{
-                    onResume()
-                }
-            }
-        )
-    }
-
-    override fun onKeywordDetected(speechText: String){
+    override fun onKeywordDetected(speechText: String) {
         Log.d("testing", "speechText: $speechText")
         Toast.makeText(this, "Keyword detected: $speechText", Toast.LENGTH_SHORT).show()
         videoService?.saveImagesToMediaStore()
     }
 
+    // Implement the onMapReady method from OnMapReadyCallback
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        val startLocation = getInitialLocation()
+        setMapStartLocation(googleMap, startLocation)
+    }
 }
---
+++ app/src/main/java/com/example/voicecam/MapsUtils.kt
@@ -0,0 +1,171 @@
+package com.example.voicecam
+
+import android.content.Context
+import android.provider.Settings.System.getString
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.Locale
+
+fun getInitialLocation(): LatLng {
+    return LatLng(37.7749, -122.4194)
+}
+
+fun setMapStartLocation(googleMap: GoogleMap, location: LatLng){
+    googleMap.addMarker(MarkerOptions().position(location).title("Starting Point"))
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+}
+
+
+suspend fun searchLocationAndDrawRoute(context: Context, location: String, googleMap: GoogleMap){
+    if (location.isEmpty()){
+        withContext(Dispatchers.Main) {
+            Toast.makeText(context, "Please enter a location to search", Toast.LENGTH_SHORT).show()
+        }
+        return
+    }
+
+    val geocoder = android.location.Geocoder(context, Locale.getDefault())
+    val addresses = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+        geocoder.getFromLocationName(location, 1) // New suspend function in API level 33
+    } else {
+        @Suppress("DEPRECATION")
+        geocoder.getFromLocationName(location, 1)
+    }
+
+    if (addresses != null && addresses.isNotEmpty()) {
+        val destinationAddress = addresses[0]
+        val destination = LatLng(destinationAddress.latitude, destinationAddress.longitude)
+        withContext(Dispatchers.Main) {
+            drawRoute(context, googleMap, getInitialLocation(), destination)
+        }
+    }else {
+        withContext(Dispatchers.Main) {
+            Toast.makeText(context, "Location not found", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+suspend fun drawRoute(context: Context, googleMap: GoogleMap, start: LatLng, end: LatLng){
+    val apiKey = context.resources.getString(R.string.google_maps_key)
+    val route = withContext(Dispatchers.IO) {
+        getRoute(start, end, apiKey)
+    }
+
+    if (route != null) {
+        val jsonObject = JSONObject(route)
+        val status = jsonObject.optString("status")
+        if (status == "ZERO_RESULTS") {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "No routes found for the given locations", Toast.LENGTH_SHORT).show()
+                return@withContext
+            }
+        }
+
+        val routePoints = parseDirectionJson(route)
+        withContext(Dispatchers.Main) {
+            googleMap.clear()
+            googleMap.addMarker(MarkerOptions().position(start).title("Start"))
+            googleMap.addMarker(MarkerOptions().position(end).title("Destination"))
+            if (routePoints.isNotEmpty()) {
+                googleMap.addPolyline(PolylineOptions().addAll(routePoints).width(10f).color(android.graphics.Color.BLUE))
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(start, 12f))
+            } else {
+                Toast.makeText(context, "Unable to draw route", Toast.LENGTH_SHORT).show()
+            }
+        }
+    } else {
+        withContext(Dispatchers.Main) {
+            Toast.makeText(context, "Error fetching route data", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+suspend fun getRoute(startLocation: LatLng, endLocation: LatLng, apiKey: String): String? {
+    val url =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${startLocation.latitude},${startLocation.longitude}&destination=${endLocation.latitude},${endLocation.longitude}&key=$apiKey"
+
+    Log.d("MapsUtils", "API request: $url") // Log the response
+    return withContext(Dispatchers.IO) {
+        try {
+            val conn = URL(url).openConnection() as HttpURLConnection
+            conn.connect()
+            val inputStream = conn.inputStream
+            val response = inputStream.bufferedReader().use { it.readText() }
+            Log.d("MapsUtils", "API Response: $response") // Log the response
+            response
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
+
+fun parseDirectionJson(jsonData: String): List<LatLng> {
+    val jsonObject = JSONObject(jsonData)
+    val routes = jsonObject.optJSONArray("routes")
+
+    if (routes == null || routes.length() == 0) {
+        Log.e("MapsUtils", "No routes found in the JSON response")
+        return emptyList()
+    }
+
+    val overviewPolyline = routes.getJSONObject(0).optJSONObject("overview_polyline")
+    val points = overviewPolyline?.optString("points")
+
+    if (points.isNullOrEmpty()) {
+        Log.e("MapsUtils", "No points found in the JSON response")
+        return emptyList()
+    }
+
+    return decodePolyline(points)
+}
+
+fun decodePolyline(encoded: String): List<LatLng> {
+    val poly = ArrayList<LatLng>()
+    var index = 0
+    val len = encoded.length
+    var lat = 0
+    var lng = 0
+
+    while (index < len) {
+        var b: Int
+        var shift = 0
+        var result = 0
+        do {
+            b = encoded[index++].code - 63
+            result = result or (b and 0x1f shl shift)
+            shift += 5
+        } while (b >= 0x20)
+        val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+        lat += dlat
+
+        shift = 0
+        result = 0
+        do {
+            b = encoded[index++].code - 63
+            result = result or (b and 0x1f shl shift)
+            shift += 5
+        } while (b >= 0x20)
+        val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+        lng += dlng
+
+        val latLng = LatLng(lat / 1E5, lng / 1E5)
+        poly.add(latLng)
+    }
+
+    return poly
+}
